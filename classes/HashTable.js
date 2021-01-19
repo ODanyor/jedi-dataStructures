9d68757e4879f3d785a8df6047ac564d925ababd
Doniyor Otamurodov <doniyorgit@gmail.com>
2021-01-19 18:58:08 +0500

    update

---
+++ classes/HashTable.js
@@ -0,0 +1,5 @@
+export default class HashTable {
+  constructor () {
+    this.buckets = new Array(100);
+  }
+}
--- scripts.js
+++ scripts.js
@@ -2,15 +2,10 @@ import LinkedList from "./classes/LinkedList.js";
 import Stack from "./classes/Stack.js";
 import Queue from "./classes/Queue.js";
 
+import HashTable from "./classes/HashTable.js";
+
 function main () {
-  const queue = new Queue();
-
-  queue.enqueue("person1");
-  queue.enqueue("person2");
-  queue.enqueue("person3");
-  queue.dequeue();
-
-  console.log(queue.toArray());
+  const hashTable = new HashTable();
 }
 
 window.addEventListener("load", main);
